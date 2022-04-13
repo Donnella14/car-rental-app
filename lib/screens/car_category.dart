507d03ed00c09e612a35838320e848aa6981fe7a
Donnella14 <mugishadonnella14@gmail.com>
2022-04-13 07:42:08 -0700

    mifinal changes

---
+++ lib/screens/car_category.dart
@@ -0,0 +1,15 @@
+// RaisedButton(
+//               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+//               disabledColor: Theme.of(context).accentColor,
+//               child: GestureDetector(
+//                       onTap: () {
+//                         Navigator.of(context).pushNamed('/cars');
+//                       },child: Text('Book Now',style: TextStyle(fontSize:20,color: Colors.white))
+//               ))
+
+// RaisedButton(
+//               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+//               disabledColor: Theme.of(context).accentColor,
+//               onPressed: () => print("Car booked"),child: Text('Book Now',style: TextStyle(fontSize:20,color: Colors.white))
+//               )
+            
